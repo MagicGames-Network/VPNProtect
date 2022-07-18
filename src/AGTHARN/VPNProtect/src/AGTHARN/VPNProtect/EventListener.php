4b2a026e5d4068a21bad5ecd64369641bd505245
AGTHARN <supscrusjus@gmail.com>
2022-07-18 17:36:04 +0800

    Make use of default arg

--- src/AGTHARN/VPNProtect/src/AGTHARN/VPNProtect/EventListener.php
+++ src/AGTHARN/VPNProtect/src/AGTHARN/VPNProtect/EventListener.php
@@ -23,17 +23,17 @@ class EventListener implements Listener
         $player = $event->getPlayer();
         if (!$player->hasPermission("vpnprotect.bypass")) {
             $this->plugin->getServer()->getAsyncPool()->submitTask(new AsyncCheckTask($player->getNetworkSession()->getIp(), $player->getName(), [
-                'check2.key' => $this->plugin->getConfig()->getNested('check2.key') ?? '',
-                'check4.key' => $this->plugin->getConfig()->getNested('check4.key') === '' ? '' : '/' . $this->plugin->getConfig()->getNested('check4.key'),
-                'check5.key' => $this->plugin->getConfig()->getNested('check5.key') ?? 'demo',
-                'check7.key' => $this->plugin->getConfig()->getNested('check7.key') ?? 'demo',
-                'check7.mobile' => $this->plugin->getConfig()->getNested('check7.mobile') ?? true,
-                'check7.fast' => $this->plugin->getConfig()->getNested('check7.fast') ?? false,
-                'check7.strictness' => $this->plugin->getConfig()->getNested('check7.strictness') ?? 0,
-                'check7.lighter_penalties' => $this->plugin->getConfig()->getNested('check7.lighter_penalties') ?? true,
-                'check8.key' => $this->plugin->getConfig()->getNested('check8.key') ?? '',
-                'check9.key' => $this->plugin->getConfig()->getNested('check9.key') ?? '',
-                'check10.key' => $this->plugin->getConfig()->getNested('check10.key') ?? ''
+                'check2.key' => $this->plugin->getConfig()->getNested('check2.key', ''),
+                'check4.key' => $this->plugin->getConfig()->getNested('check4.key', ''),
+                'check5.key' => $this->plugin->getConfig()->getNested('check5.key', 'demo'),
+                'check7.key' => $this->plugin->getConfig()->getNested('check7.key', 'demo'),
+                'check7.mobile' => $this->plugin->getConfig()->getNested('check7.mobile', true),
+                'check7.fast' => $this->plugin->getConfig()->getNested('check7.fast', false),
+                'check7.strictness' => $this->plugin->getConfig()->getNested('check7.strictness', 0),
+                'check7.lighter_penalties' => $this->plugin->getConfig()->getNested('check7.lighter_penalties', true),
+                'check8.key' => $this->plugin->getConfig()->getNested('check8.key', ''),
+                'check9.key' => $this->plugin->getConfig()->getNested('check9.key', ''),
+                'check10.key' => $this->plugin->getConfig()->getNested('check10.key', '')
             ]));
         }
     }
